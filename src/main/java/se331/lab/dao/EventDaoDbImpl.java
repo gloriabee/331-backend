6c5101d36a57c3196a269699b69178e6591753fc
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-09-07 16:47:33 +0700

    create event table in database

--- src/main/java/se331/lab/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/dao/EventDaoDbImpl.java
@@ -1,6 +1,7 @@
 package se331.lab.dao;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import se331.lab.entity.Event;
 import se331.lab.repository.EventRepository;
@@ -9,21 +10,21 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
+@Profile("db")
 
-public class EventDaoDblmpl implements EventDao {
+public class EventDaoDbImpl implements EventDao {
     final EventRepository eventRepository;
     @Override
     public Integer getEventSize(){
-
         return Math.toIntExact(eventRepository.count());
     }
 
     @Override
     public List<Event> getEvents(Integer pageSize, Integer page){
         List<Event> events=eventRepository.findAll();
-        pageSize=pageSize==null?events.size():pageSize;
-        page=page==null?1:page;
-        int firstIndex=(page-1)*pageSize;
+        pageSize = pageSize == null ? events.size() : pageSize;
+        page= page==null ? 1 : page;
+        int firstIndex= (page-1) * pageSize;
         List<Event> output=events.subList(firstIndex, firstIndex+pageSize);
         return output;
     }
